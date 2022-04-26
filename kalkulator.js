8733ff7f7c2a21ae3bb4b70f2e2c44c364754994
norshiii <matsbrenn@gmail.com>
2022-04-26 22:30:41 +0200

    version1.3

--- kalkulator.js
+++ kalkulator.js
@@ -6,13 +6,16 @@ function kalkulator() {
     inneTemp = document.getElementById("itemp").value;
     dager = document.getElementById("dager").value;
 
-    uteTempK = uteTemp + 273.15;
-    inneTempK = inneTemp + 273.15;
+    differanse  = inneTemp - uteTemp;
+    snittpris = 1.648; /* kr */
 
-    differanse  = inneTempK - uteTempK;
+    varmetapGammelResultat = areal * uVerdiGammel * differanse * dager * 24 / 1000;
+    varmetapNyResultat = areal * uVerdiNy * differanse * dager * 24 / 1000;
 
-    document.getElementById("varmetapGammelRes").innerHTML = "Med de gamle vinduene dine ville du brukt " + (areal * uVerdiGammel * differanse * dager * 24 / 1000) + " kwh på " + dager + " dager";
-    document.getElementById("varmetapNyRes").innerHTML = areal * uVerdiNy * differanse * dager * 24 / 1000 + " kwhf";
+    document.getElementById("varmetapGammelRes").innerHTML = "mens de gamle vinduene dine ville brukt " + (areal * uVerdiGammel * differanse * dager * 24 / 1000) + " kwh på samme tidsrom per vindu";
+    document.getElementById("varmetapNyRes").innerHTML = "Om du bytter vinduer vil du bruke " + (areal * uVerdiNy * differanse * dager * 24 / 1000) + " kwh mindre på " + dager + " dager";
+    wattDiff = varmetapGammelResultat - varmetapNyResultat; /* i kwh */
+    document.getElementById("fortjeneste").innerHTML = "Du vil da spare " + (wattDiff * 1.648) + "kr på " + dager + " dager" + "",""
 }
 
 function temperaturdiff() {
@@ -20,19 +23,3 @@ function temperaturdiff() {
     uteTemp = document.getElementById("utemp").value;
     document.getElementById("tempdiff").innerHTML = inneTemp - uteTemp + " °C forskjell mellom inne- og ute-temperatur";
 }
-
-
-/*prototype 2*/
-/*
-function kalkulator() {
-    areal = document.getElementById("areal").value;
-    inneTemp = document.getElementById("itemp").value;
-    uteTemp = document.getElementById("utemp").value;
-    uVerdiGammel = document.getElementById("uverdiGammel").value;
-    dager = document.getElementById("dager").value;
-    uVerdiNy = document.getElementById("uverdiNy").value;
-    differanse = inneTemp - uteTemp;
-    document.getElementById("varmetapGammelRes").innerHTML = areal * uVerdiGammel * differanse * 24 / 1000 + " kWh";
-}
-
-*/
