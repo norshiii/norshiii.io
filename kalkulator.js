bf3833efcd2181fb87ff3d25605cb28e37e4e90d
norshiii <matsbrenn@gmail.com>
2022-04-26 22:49:53 +0200

    version_innlevering

--- kalkulator.js
+++ kalkulator.js
@@ -12,14 +12,8 @@ function kalkulator() {
     varmetapGammelResultat = areal * uVerdiGammel * differanse * dager * 24 / 1000;
     varmetapNyResultat = areal * uVerdiNy * differanse * dager * 24 / 1000;
 
-    document.getElementById("varmetapGammelRes").innerHTML = "mens de gamle vinduene dine ville brukt " + (areal * uVerdiGammel * differanse * dager * 24 / 1000) + " kwh på samme tidsrom per vindu";
-    document.getElementById("varmetapNyRes").innerHTML = "Om du bytter vinduer vil du bruke " + (areal * uVerdiNy * differanse * dager * 24 / 1000) + " kwh mindre på " + dager + " dager";
+    document.getElementById("varmetapGammelRes").innerHTML = "mens de gamle vinduene dine ville brukt " + (areal * uVerdiGammel * differanse * dager * 24 / 1000) + " kwh på samme tidsrom per vindu."; /*regner ut varmetapet i kwh på gamle config av vindu*/
+    document.getElementById("varmetapNyRes").innerHTML = "Om du bytter vinduer vil du bruke " + (areal * uVerdiNy * differanse * dager * 24 / 1000) + " kwh mindre på " + dager + " dager"; /*regner ut varmetap på ny config av vindu*/
     wattDiff = varmetapGammelResultat - varmetapNyResultat; /* i kwh */
-    document.getElementById("fortjeneste").innerHTML = "Du vil da spare " + (wattDiff * 1.648) + "kr på " + dager + " dager" + "",""
-}
-
-function temperaturdiff() {
-    inneTemp = document.getElementById("itemp").value;
-    uteTemp = document.getElementById("utemp").value;
-    document.getElementById("tempdiff").innerHTML = inneTemp - uteTemp + " °C forskjell mellom inne- og ute-temperatur";
+    document.getElementById("fortjeneste").innerHTML = "Du vil da spare ca." + (wattDiff * 1.648) + "kr på " + dager + " dager" /*ca mengde med penger spart*/
 }
